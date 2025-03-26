0cb4cf77e3aabdefd8b2dd1bcf8a881e30ba848b
Stefania <tudor.stefania.miruna.2003@gmail.com>
2025-03-27 00:49:14 +0200

    SCRUM-25 Create Database Tied all tables

---
+++ CatalogueApp/CatalogueServer/Models/DatabaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SQLite;
+
+public class Database
+{
+    private readonly SQLiteConnection _db;
+
+    public Database()
+    {
+        string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "school_database.db");
+        _db = new SQLiteConnection(dbPath);
+
+        // Enable foreign keys
+        _db.Execute("PRAGMA foreign_keys = ON;");
+
+        // Create tables
+        _db.CreateTable<User>();
+        _db.CreateTable<Class>();
+        _db.CreateTable<Grade>();
+        _db.CreateTable<Assignment>();
+    }
+
+    public SQLiteConnection GetConnection() => _db;
+}
