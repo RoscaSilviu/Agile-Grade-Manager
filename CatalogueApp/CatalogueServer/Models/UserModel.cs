be53a3d07fb24edb55ae562984062d308c464182
Stefania <tudor.stefania.miruna.2003@gmail.com>
2025-03-26 23:40:05 +0200

    SCRUM-28 Setup Database Create Users Table

---
+++ CatalogueApp/CatalogueServer/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+[Table("Users")]
+public class User
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    public string Password { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    //The email will be used as the username -> unique
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public string Token { get; set; }
+    public DateTime LastLogin { get; set; }
+}
