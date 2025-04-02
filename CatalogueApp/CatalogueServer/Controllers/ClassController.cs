2cb09bb23063b3f9bcb2cbc100c53d62396bbfd5
Stefania <tudor.stefania.miruna.2003@gmail.com>
2025-04-02 20:03:12 +0300

    SCRUM-44 Created Class Controller and The Teacher Main Page

---
+++ CatalogueApp/CatalogueServer/Controllers/ClassController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CatalogueServer.Repositories;
+
+namespace CatalogueServer.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        private readonly ClassRepository _classRepository;
+
+        public ClassController(ClassRepository classRepository)
+        {
+            _classRepository = classRepository;
+        }
+
+        [HttpGet]
+        //get all the classes with the teacher id
+        public IActionResult GetClasses(int teacherId)
+        {
+            var classes = _classRepository.GetClassesByTeacherId(teacherId);
+            return Ok(classes);
+        }
+
+
+    }
+}
